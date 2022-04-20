934ed7ba7b09d2d8604c999061bffd5dba878f47
Eric Wang <ewang1997@gmail.com>
2022-04-20 11:21:26 +0300

    implement circle and ellipse

--- build/index.js
+++ build/index.js
@@ -5,6 +5,11 @@ function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" =
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.drawCircle = drawCircle;
+exports.drawEllipse = drawEllipse;
+exports.drawRectangle = drawRectangle;
+exports.fillCircle = fillCircle;
+exports.fillEllipse = fillEllipse;
 exports.fillRectangle = fillRectangle;
 Object.defineProperty(exports, "isKeyPressed", {
   enumerable: true,
@@ -96,6 +101,67 @@ function runGraphics(startWorld, updateWorld, drawWorld) {
   setInterval(iterateGraphics, 10);
 }
 
+function drawCircle(x, y, radius) {
+  var color = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : "black";
+  var thickness = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : 1;
+  window._graphics.ctx.strokeStyle = color;
+  window._graphics.ctx.lineWidth = thickness;
+
+  window._graphics.ctx.beginPath();
+
+  window._graphics.ctx.ellipse(x, y, radius, radius, 0, 0, 2 * Math.PI);
+
+  window._graphics.ctx.stroke();
+}
+
+function fillCircle(x, y, radius) {
+  var color = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : "black";
+  window._graphics.ctx.fillStyle = color;
+
+  window._graphics.ctx.beginPath();
+
+  window._graphics.ctx.ellipse(x, y, radius, radius, 0, 0, 2 * Math.PI);
+
+  window._graphics.ctx.fill();
+}
+
+function drawEllipse(x, y, width, height) {
+  var color = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : "black";
+  var thickness = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : 1;
+  window._graphics.ctx.strokeStyle = color;
+  window._graphics.ctx.lineWidth = thickness;
+
+  window._graphics.ctx.beginPath();
+
+  window._graphics.ctx.ellipse(x, y, width / 2, height / 2, 0, 0, 2 * Math.PI);
+
+  window._graphics.ctx.stroke();
+}
+
+function fillEllipse(x, y, width, height) {
+  var color = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : "black";
+  window._graphics.ctx.fillStyle = color;
+
+  window._graphics.ctx.beginPath();
+
+  window._graphics.ctx.ellipse(x, y, width / 2, height / 2, 0, 0, 2 * Math.PI);
+
+  window._graphics.ctx.fill();
+}
+
+function drawRectangle(x, y, width, height) {
+  var color = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : "black";
+  var thickness = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : 1;
+  window._graphics.ctx.strokeStyle = color;
+  window._graphics.ctx.lineWidth = thickness;
+  console.log(color, thickness);
+
+  window._graphics.ctx.strokeRect(x, y, width, height);
+}
+
 function fillRectangle(x, y, width, height) {
+  var color = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : "black";
+  window._graphics.ctx.fillStyle = color;
+
   window._graphics.ctx.fillRect(x, y, width, height);
 }
--- src/index.js
+++ src/index.js
@@ -34,7 +34,45 @@ export function runGraphics(startWorld, updateWorld, drawWorld) {
   setInterval(iterateGraphics, 10)
 }
 
-export function fillRectangle(x, y, width, height) {
+export function drawCircle(x, y, radius, color="black", thickness=1) {
+  window._graphics.ctx.strokeStyle = color
+  window._graphics.ctx.lineWidth = thickness
+  window._graphics.ctx.beginPath();
+  window._graphics.ctx.ellipse(x, y, radius, radius, 0, 0, 2*Math.PI);
+  window._graphics.ctx.stroke();
+}
+
+export function fillCircle(x, y, radius, color="black") {
+  window._graphics.ctx.fillStyle = color
+  window._graphics.ctx.beginPath();
+  window._graphics.ctx.ellipse(x, y, radius, radius, 0, 0, 2*Math.PI);
+  window._graphics.ctx.fill();
+}
+
+export function drawEllipse(x, y, width, height, color="black", thickness=1) {
+  window._graphics.ctx.strokeStyle = color
+  window._graphics.ctx.lineWidth = thickness
+  window._graphics.ctx.beginPath();
+  window._graphics.ctx.ellipse(x, y, width/2, height/2, 0, 0, 2*Math.PI);
+  window._graphics.ctx.stroke();
+}
+
+export function fillEllipse(x, y, width, height, color="black") {
+  window._graphics.ctx.fillStyle = color
+  window._graphics.ctx.beginPath();
+  window._graphics.ctx.ellipse(x, y, width/2, height/2, 0, 0, 2*Math.PI);
+  window._graphics.ctx.fill();
+}
+
+export function drawRectangle(x, y, width, height, color="black", thickness=1) {
+  window._graphics.ctx.strokeStyle = color;
+  window._graphics.ctx.lineWidth = thickness;
+  console.log(color, thickness)
+  window._graphics.ctx.strokeRect(x, y, width, height)
+}
+
+export function fillRectangle(x, y, width, height, color="black") {
+  window._graphics.ctx.fillStyle = color;
   window._graphics.ctx.fillRect(x, y, width, height)
 }
 
